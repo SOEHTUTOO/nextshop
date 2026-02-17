a9ad86764f20f8b94d42b0dbc6b21269d2476860
Soe Htut Oo <usoehtutoo@gmail.com>
2026-02-17 11:36:37 +0630

    05: search filter

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -1,12 +1,37 @@
+import { unique } from 'next/dist/build/utils'
 import type { CollectionConfig } from 'payload'
 
 export const Categories: CollectionConfig = {
   slug: 'categories',
   fields: [
     {
-            name: 'name',
-            type: 'text',
-            required: true,
-        },
+      name: 'name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'slug',
+      type: "text",
+      required: true,
+      unique: true,
+      index: true,
+    },
+    {
+      name: "color",
+      type: "text",
+    },
+    {
+      name: "parent",
+      type: "relationship",
+      relationTo: "categories",
+      hasMany: false,
+    },
+    {
+      name: "subcategoreis",
+      type: "join",
+      collection: "categories",
+      on: "parent",
+      hasMany: true,
+    },
   ],
 }
