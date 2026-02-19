33ded15e8d011f90e91180f1033f30d46798ce60
Soe Htut Oo <usoehtutoo@gmail.com>
2026-02-19 21:43:13 +0630

    07: trpc fix\

--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -1,8 +1,9 @@
+import superjson from "superjson";
 import {
   defaultShouldDehydrateQuery,
   QueryClient,
 } from '@tanstack/react-query';
-// import superjson from 'superjson';
+
 export function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
@@ -10,7 +11,7 @@ export function makeQueryClient() {
         staleTime: 30 * 1000,
       },
       dehydrate: {
-        // serializeData: superjson.serialize,
+        serializeData: superjson.serialize,
         shouldDehydrateQuery: (query) =>
           defaultShouldDehydrateQuery(query) ||
           query.state.status === 'pending',
