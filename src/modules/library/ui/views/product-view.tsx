0b82017f6d5be3e436e1591c35696ed9ca6c534e
Soe Htut Oo <usoehtutoo@gmail.com>
2026-03-03 02:47:05 +0630

    24: Access control

--- src/modules/library/ui/views/product-view.tsx
+++ src/modules/library/ui/views/product-view.tsx
@@ -40,9 +40,15 @@ export const ProductView = ({ productId }: Props) => {
             </div>
 
             <div className="lg:col-span-5">
-                <p className="font-medium italic text-muted-foreground">
+                {data.content ? 
+                    <p>
+                        {data.content}
+                    </p>
+                : (
+                    <p className="font-medium italic text-muted-foreground">
                     No special conetent
                 </p>
+                )}
             </div>
 
            </div>
