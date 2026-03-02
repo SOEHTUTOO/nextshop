a0a98d74ffeb082662e9f6f789480285f8110c65
Soe Htut Oo <usoehtutoo@gmail.com>
2026-03-03 04:47:03 +0630

    25: Stripe connect

---
+++ src/components/stripe-verify.tsx
@@ -0,0 +1,13 @@
+import { Button, Link } from "@payloadcms/ui";
+
+export const StripeVerify = () => {
+    return (
+        <Link href="/stripe-verify">
+            <Button>
+                Verify Account
+            </Button>
+        </Link>
+    );
+};
+
+export default StripeVerify;
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const DEFAULT_LIMIT = 8;
 export const SHOP_NAME = "nextshop";
 export const SHOP_DESCRIPTION = "nextshop, the best shop";
+export const PLATFORM_FEE_PERCENTATE = 10;
